fe4a6d12c2a4627f19d2730a92d84350c21ad073
unknown <okanylmz>
2019-10-13 22:06:48 +0300

    Updates

---
+++ precache-manifest.9103904507be96a34f9853b28e26c8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f41ab8f97f81a96e0184c6fcd29fdd1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dec9a0b48472a882417d",
+    "url": "/monster-rolodex/static/css/main.4db31dbb.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monster-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "dec9a0b48472a882417d",
+    "url": "/monster-rolodex/static/js/main.22f80fcc.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
